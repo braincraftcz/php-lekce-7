75eb365fe085ac5d53b03017acc37cb148bc830c
Luděk Veselý <ludek.vesely@email.com>
2019-12-05 15:09:38 +0100

    Lekce 8, domácí úkol 1 - procvičení

---
+++ src/Controller/HomeworkController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeworkController extends AbstractController
+{
+    /**
+     * @Route("/homework", name="homework")
+     */
+    public function index()
+    {
+        return $this->render('homework/index.html.twig', [
+            'controller_name' => '8. lekce, 21.11.2019',
+        ]);
+    }
+
+    /**
+     * @Route("/homework/remember", name="remember")
+     */
+    public function remember()
+    {
+        return $this->render('homework/remember.html.twig', [
+            'controller_name' => 'HomeworkController',
+        ]);
+    }
+
+    /**
+     * @Route("/homework/list", name="list")
+     */
+    public function list()
+    {
+        return $this->render('homework/list.html.twig', [
+            'controller_name' => 'HomeworkController',
+            'pupils' => [
+                [
+                    'name' => 'Josef',
+                    'grades' => [
+                        [
+                            'subject' => 'Biologie',
+                            'grade' => 1
+                        ],
+                        [
+                            'subject' => 'Anglický jazyk',
+                            'grade' => 4
+                        ]
+                    ]
+                ],
+                [
+                    'name' => 'Karel',
+                    'grades' => [
+                        [
+                            'subject' => 'Matematika',
+                            'grade' => 2
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
+}
